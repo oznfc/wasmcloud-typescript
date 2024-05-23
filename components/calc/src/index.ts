1f091afb54b3f5ef9aaa36453f99369f89e12e16
Lachlan Heywood <lachieh@users.noreply.github.com>
2024-05-23 11:33:09 -0400

    update with additional node_module

---
+++ components/calc/src/index.ts
@@ -0,0 +1,8 @@
+export function calculatesum(x: number, y: number): number {
+  // console.log("calculatesum", x, y);
+  return x + y;
+}
+
+export const calculator = {
+  calculatesum,
+}
--- dist/src/index.js
+++
@@ -1,4 +0,0 @@
-export function calculatesum(x, y) {
-    // console.log("calculatesum", x, y);
-    return x + y;
-}

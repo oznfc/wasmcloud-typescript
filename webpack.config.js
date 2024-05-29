f2da3e82bfc03d342cd20195624a10bdeef6a4c7
oznfc <koc651@gmail.com>
2024-05-29 12:04:01 +0300

    tried to add async

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,23 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.ts',
+    mode: 'production',
+    // import the main files for each component as entry points
+    entry: {
+        'http-hello-world':'./components/http-hello-world/src/index.ts',
+        calc:'./components/calc/src/index.ts',
+    },
+    // mark the component modules as externals as they will be provided by other components at runtime
+    externals: {
+        "wasi:http/types@0.2.0": 'wasi:http/types@0.2.0',
+        "wasmcloud:hello/calculator": 'wasmcloud:hello/calculator',
+        "wasi:logging/logging":'wasi:logging/logging',
+    },
+    // mark the output as a module as wasi expects to use esmodule syntax
+    externalsType: 'module',
+    experiments: {outputModule: true},
+
+    // run the files through the typescript and babel loaders
     module: {
         rules: [
             {
@@ -21,11 +37,17 @@ module.exports = {
             }
         ],
     },
+    optimization: {
+        minimize: false,
+    },
     resolve: {
         extensions: ['.tsx', '.ts', '.js'],
     },
     output: {
-        filename: 'bundle.js',
+        // output each component to it's own file
+        filename: '[name].js',
         path: path.resolve(__dirname, 'dist'),
+        // make sure the output is compatible as an esmodule
+        library: { type: 'module' },
     },
 };
